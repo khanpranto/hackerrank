1f0dd2ffef296c47aca3edd2a1a85899255853cb
khanpranto <97869460+khanpranto@users.noreply.github.com>
2022-07-25 16:52:54 +0600

    Add files via upload

---
+++ hackerrank2.py
@@ -0,0 +1,10 @@
+a = int(input('Enter the 1st number:'))
+b = int(input('Enter the 2nd number:'))
+
+sum = a + b
+diffrence = a - b
+multiplication = a * b
+
+print(sum)
+print(diffrence)
+print(multiplication)
